ddfda6cc2eb9f4f288e5199c2020b78090c6c646
Mr.Zhao <zhaoyc128@163.com>
2017-10-22 16:43:00 +0800

    产品列表
    获取产品列表，批量更新产品

--- apps/aliexpress/urls.py
+++ apps/aliexpress/urls.py
@@ -4,8 +4,11 @@ from __future__ import unicode_literals
 from apps.aliexpress.views import *
 urls = [
     (r"orderList$", SMTOrderListHandler),
+    (r"productList$", SMTProductListHandler),
     (r"api/checkOrder$", SMTCheckOrderHandler),
     (r"api/refreshOrderStatus$", SMTRefreshOrderStatusHandler),
     (r"api/refreshOrderInfos$", SMTRefreshOrderInfosHandler),
     (r"api/checkProduct$", SMTCheckProductHandler),
+    (r"api/refreshProductStatus$", SMTRefreshProductStatusHandler),
+    (r"api/refreshProductInfos$", SMTRefreshProductInfosHandler),
 ]
--- apps/aliexpress/views.py
+++ apps/aliexpress/views.py
@@ -154,6 +154,122 @@ class SMTOrderListHandler(BaseHandler):
             self.render('error/message.html', msg={'Msg': 'No Permission', 'Code': 400,'Title':'无权限！','Link':'/'})
 
 
+class SMTProductListHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+
+        AUTHOR_MOUDLE = 'ViewSMTProduct'
+
+        user = self.current_user
+        role = self.get_secure_cookie("role") if self.get_secure_cookie("role") else 'None'
+
+        mongo = MongoCase()
+        mongo.connect()
+        client = mongo.client
+
+        db = client.woderp
+
+        account = db.user.find_one({'account':user})
+
+        authority = self.getAuthority(account,AUTHOR_MOUDLE)
+
+        if authority['Allow']:
+
+            pageSize = 200
+
+            status = self.get_argument('status','')
+            store = self.get_argument('store','')
+            wd = self.get_argument('wd','')
+            platform = self.get_argument('platform','aliexpress')
+
+            try:
+                page = int(self.get_argument('page',1))
+            except:
+                page = 1
+
+            #totalCount = db.orderList.find({"order_state":"WAIT_SELLER_STOCK_OUT"}).count()
+            option = {'platform':platform}
+
+            matchOption = dict()
+
+            if authority['role'] == 'Supper':
+                appList = db.appList.find({'platform': 'aliexpress'})
+            else:
+                appList = db.appList.find({'platform': 'aliexpress','storeId':{'$in':authority['authority']['smtStore']}})
+
+
+            if status != '':
+                option['productStatusType'] = status
+                matchOption['productStatusType'] = status
+
+            if store != '':
+                option['storeInfo.storeId'] = store
+                matchOption['storeInfo.storeId'] = store
+            elif authority['role'] != 'Supper' :
+                option['storeInfo.storeId'] = {'$in':authority['authority']['smtStore']}
+                matchOption['storeInfo.storeId'] = {'$in':authority['authority']['smtStore']}
+
+            statusList = db.productList.aggregate([{ '$match' : matchOption },{'$group': {'_id': "$productStatusType", 'Count': {'$sum': 1}}}])
+
+            sL = []
+            for s in statusList:
+                if s['_id']:
+                    stxt = ''
+                    if s['_id'] == 'onSelling':
+                        stxt += '上架'
+                    elif s['_id'] == 'offline':
+                        stxt += '下架'
+                    elif s['_id'] == 'auditing':
+                        stxt += '审核中'
+                    elif s['_id'] == 'editingRequired':
+                        stxt += '审核不通过'
+                    sL.append({'status': s['_id'], 'Count': s['Count'], 'statusTxt': stxt})
+
+
+            if wd != '':
+                words = re.compile(wd)
+
+                filerList = []
+                filerList.append({'subject':words})
+                filerList.append({'productId':words})
+
+                option['$or'] = filerList
+
+
+
+            totalCount = db.productList.find(option).count()
+
+            productData = db.productList.find(option).sort("gmtCreate",-1).limit(pageSize).skip((page-1)*pageSize)
+
+            p = divmod(totalCount,pageSize)
+
+            pageInfo = dict()
+
+            totalPage = p[0]
+            if p[1]>0:
+                totalPage += 1
+
+            pageInfo['totalPage'] = totalPage
+            pageInfo['totalCount'] = totalCount
+            pageInfo['pageSize'] = pageSize
+            pageInfo['pageNo'] = page
+            pageInfo['pageList'] = range(1,totalPage+1)
+
+            filterData = dict()
+            filterData['status'] = status
+            filterData['store'] = store
+            filterData['wd'] = wd
+            filterData['statusList'] = sL
+            filterData['appList'] = appList
+
+            self.render('smt/product-list.html',productData = productData,pageInfo = pageInfo,filterData=filterData,userInfo={'account':user,'role':role})
+
+            #self.render('index.html')
+
+        else:
+            #self.write("No Permission")
+            self.render('error/message.html', msg={'Msg': 'No Permission', 'Code': 400,'Title':'无权限！','Link':'/'})
+
 
 class SMTCheckOrderHandler(BaseHandler):
     @tornado.web.asynchronous
@@ -215,3 +331,31 @@ class SMTCheckProductHandler(BaseHandler):
         self.write(result)
         self.finish()
 
+
+class SMTRefreshProductStatusHandler(BaseHandler):
+    @tornado.web.asynchronous
+    @tornado.gen.engine
+    def get(self):
+
+        items = self.get_argument('items', '')
+        url = apiServer+"/smt/api/refreshProductStatus?" +urllib.urlencode({'items':items})
+        request = HTTPRequest(url=url,method="GET",follow_redirects=False,request_timeout=3000)
+        client = tornado.httpclient.AsyncHTTPClient()
+        response = yield tornado.gen.Task(client.fetch, request)
+        result = json.loads(response.body)
+        self.write(result)
+        self.finish()
+
+class SMTRefreshProductInfosHandler(BaseHandler):
+    @tornado.web.asynchronous
+    @tornado.gen.engine
+    def get(self):
+
+        items = self.get_argument('items', '')
+        url = apiServer+"/smt/api/refreshProductInfos?" +urllib.urlencode({'items':items})
+        request = HTTPRequest(url=url,method="GET",follow_redirects=False,request_timeout=3000)
+        client = tornado.httpclient.AsyncHTTPClient()
+        response = yield tornado.gen.Task(client.fetch, request)
+        result = json.loads(response.body)
+        self.write(result)
+        self.finish()
