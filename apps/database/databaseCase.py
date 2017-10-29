5738e1dd618edbe47ed0c4205dc101e5ea3c3d63
zhaoyoucai <zhaoyc128@163.com>
2017-10-29 23:57:36 +0800

    mysql数据库简单封装

--- apps/database/databaseCase.py
+++ apps/database/databaseCase.py
@@ -1,6 +1,11 @@
 #coding:utf-8
 
 from pymongo import MongoClient
+import MySQLdb
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 class MongoCase:
 
@@ -10,3 +15,71 @@ class MongoCase:
     def connect(self):
         if self.client == None:
             self.client = MongoClient()
+
+
+class MySQLCase:
+
+    host = 'localhost'
+    user = 'root'
+    passwd = 'zyc880128'
+    port = 3306
+    charset = 'utf8'
+    db = ''
+
+    conn = None
+
+
+    def __init__(self,db):
+        self.db = db
+
+    def connect(self):
+        if self.conn == None:
+            self.conn=MySQLdb.connect(host=self.host,
+                                      user=self.user,
+                                      passwd=self.passwd,
+                                      db=self.db,
+                                      port=self.port,
+                                      charset=self.charset)
+
+    def colse(self):
+        self.conn.close()
+        self.conn = None
+
+
+    def getData(self,sql):
+        if self.conn:
+            conn = self.conn
+        else:
+            self.connect()
+            conn = self.conn
+
+        cursor = conn.cursor()
+
+        cursor.execute(sql)
+        rawData = cursor.fetchall()
+
+        return rawData
+
+
+    def getDictData(self,sql):
+        if self.conn:
+            conn = self.conn
+        else:
+            self.connect()
+            conn = self.conn
+
+        cursor = conn.cursor()
+
+        cursor.execute(sql)
+        rawData = cursor.fetchall()
+        col_names = [desc[0] for desc in cursor.description]
+
+        result = []
+        for row in rawData:
+            objDict = {}
+            for index, value in enumerate(row):
+                objDict[col_names[index]] = str(value)
+
+            result.append(objDict)
+
+        return result
